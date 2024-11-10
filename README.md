55f06a1beb3633f97242a00dae2c88183c214fd2
gl1tchs <berkeumtbiricik@gmail.com>
2024-11-10 19:59:19 +0300

    add: make it work.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # A Scheme interpreter written in rust.
 
 ```scheme
-(define fib (lambda (n) (if (< n 2) 1 (+ (fib (- n 1)) (fib (- n 2))))))
+repl> (begin (define r 10) (set! r 5) (* pi (* r r)))
 ```
 
 A Scheme implementation in rust for mathematical calculations. It doesn't support
@@ -13,7 +13,8 @@ conditions and variables.
 - define: defines a new variable it can only be *integer, float, list or lambda*
 - if: checks for the condition if true evaluates the third argument otherwise evaluates the fourth argument.
 - begin: evaluates each expression individualy but only returns the last one.
-- apply: applies given function / operator to all elements of given list / atomic.
+- car: retrieves the first element from given list, e.g. (car a b c d) => a
+- set!: sets value to the variable, e.g. (set! a (+ 10 11))
 - lambda: defines a function with parameters, e.g. (lambda (x y) (x * y))
 - \+, add: Addition operation.
 - \-, sub: Subtraction operation.
@@ -25,7 +26,15 @@ conditions and variables.
 - abs: Absolute value, e.g., (abs x) -> |x|
 - min: Returns the smallest value in a list, e.g., (min a b c ...)
 - max: Returns the largest value in a list, e.g., (max a b c ...)
+- sin: Takes sin of given value, e.g., (sin a)
+- cos: Takes cos of given value, e.g., (cos a)
+- pi: Pi number
+- e: E number
 - \>, gt: Greater-than comparison, e.g., (> a b)
 - \<,  lt: Less-than comparison, e.g., (< a b)
 - \>\=, gte: Greater-than-or-equal comparison, e.g., (>= a b)
 - \<\=, lte: Less-than-or-equal comparison, e.g., (<= a b)
+
+## Issues
+
+- [ ] Recursive procedures are not working.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,529 @@
+use std::collections::{HashMap, VecDeque};
+use std::f32::consts::{E, PI};
+use std::io::{self, Write};
+use std::rc::Rc;
+use std::{error::Error, fmt};
+
+#[derive(Debug, Clone, PartialEq)]
+enum Expr {
+    Sym(String),
+    Num(f32),
+    List(Vec<Expr>),
+}
+
+impl ToString for Expr {
+    fn to_string(&self) -> String {
+        match &self {
+            Expr::Sym(sym) => sym.to_string(),
+            Expr::Num(num) => format!("{}", num),
+            Expr::List(list) => list.iter().map(|expr| format!("{:?}", expr)).collect(),
+        }
+    }
+}
+
+type Proc = dyn Fn(Vec<Value>) -> Result<Value, String>;
+
+#[derive(Clone)]
+enum Value {
+    Sym(String),
+    Num(f32),
+    List(Vec<Expr>),
+    Func(Rc<Proc>),
+}
+
+impl ToString for Value {
+    fn to_string(&self) -> String {
+        match &self {
+            Value::Sym(sym) => sym.to_string(),
+            Value::Num(num) => format!("{}", num),
+            Value::List(list) => list.iter().map(|expr| format!("{:?}", expr)).collect(),
+            Value::Func(proc) => format!("{:p}", proc),
+        }
+    }
+}
+
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Value::Sym(a), Value::Sym(b)) => a == b,
+            (Value::Num(a), Value::Num(b)) => a == b,
+            (Value::List(a), Value::List(b)) => a == b,
+            _ => false,
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Env {
+    outer: Option<Box<Env>>,
+    map: HashMap<String, Value>,
+}
+
+impl Env {
+    fn from(outer: Env) -> Self {
+        Self {
+            outer: Some(Box::new(outer)),
+            map: HashMap::new(),
+        }
+    }
+
+    fn get(&self, key: &str) -> Option<&Value> {
+        if let Some(value) = self.map.get(key) {
+            return Some(value);
+        }
+
+        match &self.outer {
+            Some(outer) => outer.get(key),
+            _ => None,
+        }
+    }
+
+    fn set(&mut self, key: &str, value: Value) {
+        self.map.insert(key.to_string(), value);
+    }
+
+    fn insert(&mut self, symbol: &str, value: Value) {
+        self.map.insert(symbol.into(), value);
+    }
+
+    fn insert_func(&mut self, symbol: &str, func: Rc<Proc>) {
+        self.map.insert(symbol.into(), Value::Func(func));
+    }
+}
+
+#[derive(Debug)]
+struct SyntaxError(String);
+
+impl Error for SyntaxError {}
+
+impl fmt::Display for SyntaxError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error while parsing the expression: {}", self.0)
+    }
+}
+
+fn tokenize(input: &str) -> VecDeque<String> {
+    input
+        .replace('(', "( ")
+        .replace(')', " ) ")
+        .split(' ')
+        .filter(|&s| !s.is_empty())
+        .map(|s| s.to_string())
+        .collect()
+}
+
+fn parse(input: &str) -> Result<Expr, SyntaxError> {
+    read_from_tokens(&mut tokenize(input))
+}
+
+fn read_from_tokens(tokens: &mut VecDeque<String>) -> Result<Expr, SyntaxError> {
+    if tokens.is_empty() {
+        return Err(SyntaxError("Cannot parse from an empty token list.".into()));
+    }
+
+    let token = tokens.pop_front().unwrap(); // Safe to unwrap, as we checked for emptiness
+    if &token == "(" {
+        let mut list = Vec::new();
+        while tokens.front().map(|t| t.as_str()) != Some(")") {
+            list.push(read_from_tokens(tokens)?);
+        }
+        tokens.pop_front(); // Remove ')'
+        Ok(Expr::List(list))
+    } else if &token == ")" {
+        Err(SyntaxError("Unexpected ')'".into()))
+    } else {
+        Ok(atom(&token))
+    }
+}
+
+fn atom(token: &str) -> Expr {
+    if let Ok(num) = token.parse::<f32>() {
+        Expr::Num(num)
+    } else {
+        Expr::Sym(token.to_string())
+    }
+}
+
+fn eval(x: Expr, env: &mut Env) -> Option<Value> {
+    match x {
+        Expr::Sym(sym) => env.get(&sym).cloned(),
+        Expr::Num(num) => Some(Value::Num(num)),
+        Expr::List(list) => {
+            let op = if let Expr::Sym(sym) = &list[0] {
+                Some(sym.as_str())
+            } else {
+                None
+            }?;
+
+            match op {
+                "if" => {
+                    let exp = if eval(list[1].clone(), env).unwrap() == Value::Num(1.0) {
+                        list[2].clone()
+                    } else {
+                        list[3].clone()
+                    };
+                    return eval(exp, env);
+                }
+                "define" => {
+                    if let Expr::Sym(sym) = &list[1] {
+                        let res = eval(list[2].clone(), env)?;
+                        env.set(sym, res);
+                    }
+
+                    None
+                }
+                "set!" => {
+                    let symbol = if let Expr::Sym(sym) = &list[1] {
+                        Some(sym.as_str())
+                    } else {
+                        None
+                    }?;
+
+                    // set the symbol if it can, do not do otherwise
+                    let res = eval(list[2].clone(), env)?;
+                    env.set(symbol, res);
+
+                    None
+                }
+                "lambda" => {
+                    let params = if let Expr::List(args) = list[1].clone() {
+                        Some(args)
+                    } else {
+                        None
+                    }?;
+
+                    let env = env.clone();
+
+                    let proc: Rc<Proc> = Rc::new(move |args: Vec<Value>| {
+                        if args.len() != params.len() {
+                            return Err(
+                                "Length of parameters are not suitable with the given ones."
+                                    .to_string(),
+                            );
+                        }
+
+                        let mut inner_env = Env::from(env.clone());
+                        for (i, param) in params.iter().enumerate() {
+                            if let Expr::Sym(sym) = param {
+                                let arg = args[i].clone();
+                                inner_env.insert(sym, arg);
+                            } else {
+                                return Err("parameters should be a type of symbol.".to_string());
+                            }
+                        }
+
+                        // evaluate the procedure with body and arguments
+                        if let Some(result) = eval(list[2].clone(), &mut inner_env) {
+                            return Ok(result);
+                        }
+
+                        Err("Error while executing procedure".into())
+                    });
+
+                    Some(Value::Func(proc))
+                }
+                _ => {
+                    // procedure call
+                    let proc = eval(Expr::Sym(op.into()), env)?;
+                    let vals: Vec<Value> = list
+                        .iter()
+                        .skip(1)
+                        .map(|arg| eval(arg.clone(), env))
+                        .filter_map(|arg| {
+                            if arg.is_none() {
+                                return None;
+                            }
+
+                            return Some(arg.unwrap());
+                        })
+                        .collect();
+
+                    match proc {
+                        Value::Func(proc) => Some(proc(vals).unwrap()),
+                        _ => None,
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn repl() {
+    let stdin = io::stdin();
+    let mut input = String::new();
+    let mut env = Env::default();
+
+    loop {
+        print!("repl> ");
+
+        io::stdout().flush().unwrap();
+
+        input.clear();
+        let _ = stdin.read_line(&mut input);
+
+        match parse(&input) {
+            Ok(ast) => {
+                if let Some(result) = eval(ast, &mut env) {
+                    println!("{}", result.to_string());
+                }
+            }
+            Err(err) => println!("{}", err),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    repl();
+}
+
+impl Default for Env {
+    fn default() -> Self {
+        let mut env = Self {
+            outer: None,
+            map: HashMap::new(),
+        };
+
+        env.insert_func(
+            "begin",
+            Rc::new(|args| {
+                if args.is_empty() {
+                    return Err("begin requires at least one argument.".into());
+                }
+                // Return the last argument's result
+                let last_val = &args[args.len() - 1];
+                Ok(last_val.clone())
+            }),
+        );
+
+        env.insert_func(
+            "car",
+            Rc::new(|args| {
+                if args.is_empty() {
+                    return Err("car expects at least one argument".into());
+                }
+
+                Ok(args[0].clone())
+            }),
+        );
+
+        env.insert_func(
+            "+",
+            Rc::new(|args| {
+                let sum = args.iter().try_fold(0.0, |acc, expr| match expr {
+                    Value::Num(n) => Ok::<f32, String>(acc + *n as f32),
+                    _ => Err("Addition requires numeric arguments.".into()),
+                })?;
+                Ok(Value::Num(sum))
+            }),
+        );
+        env.insert_func(
+            "-",
+            Rc::new(|args| {
+                if args.len() != 2 {
+                    return Err("Subtraction function expects two arguments.".into());
+                }
+                let a = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("Subtraction requires numeric arguments.".into()),
+                };
+                let b = match &args[1] {
+                    Value::Num(n) => *n,
+                    _ => return Err("Subtraction requires numeric arguments.".into()),
+                };
+                Ok(Value::Num(a - b))
+            }),
+        );
+        env.insert_func(
+            "*",
+            Rc::new(|args| {
+                let product = args.iter().try_fold(1.0, |acc, expr| match expr {
+                    Value::Num(n) => Ok::<f32, String>(acc * n),
+                    _ => Err("Multiplication requires numeric arguments.".into()),
+                })?;
+                Ok(Value::Num(product))
+            }),
+        );
+        env.insert_func(
+            "/",
+            Rc::new(|args| {
+                if args.len() != 2 {
+                    return Err("Division function expects two arguments.".into());
+                }
+                let a = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("Division requires numeric arguments.".into()),
+                };
+                let b = match &args[1] {
+                    Value::Num(n) => *n,
+                    _ => return Err("Division requires numeric arguments.".into()),
+                };
+                if b == 0.0 {
+                    return Err("Division by zero.".into());
+                }
+                Ok(Value::Num(a / b))
+            }),
+        );
+        env.insert_func(
+            "mod",
+            Rc::new(|args| {
+                if args.len() != 2 {
+                    return Err("mod expects two arguments.".into());
+                }
+                let a = match &args[0] {
+                    Value::Num(n) => *n as i32,
+                    _ => return Err("mod requires numeric arguments.".into()),
+                };
+                let b = match &args[1] {
+                    Value::Num(n) => *n as i32,
+                    _ => return Err("mod requires numeric arguments.".into()),
+                };
+                Ok(Value::Num((a % b) as f32))
+            }),
+        );
+        env.insert_func(
+            "expt",
+            Rc::new(|args| {
+                if args.len() != 2 {
+                    return Err("expt expects two arguments.".into());
+                }
+                let base = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("expt requires numeric arguments.".into()),
+                };
+                let exp = match &args[1] {
+                    Value::Num(n) => *n,
+                    _ => return Err("expt requires numeric arguments.".into()),
+                };
+                Ok(Value::Num(base.powf(exp)))
+            }),
+        );
+        env.insert_func(
+            "sqrt",
+            Rc::new(|args| {
+                if args.len() != 1 {
+                    return Err("sqrt expects one argument.".into());
+                }
+                let x = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("sqrt expects a numeric argument.".into()),
+                };
+                Ok(Value::Num(x.sqrt()))
+            }),
+        );
+        env.insert_func(
+            "abs",
+            Rc::new(|args| {
+                if args.len() != 1 {
+                    return Err("abs expects one argument.".into());
+                }
+                let x = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("abs expects a numeric argument.".into()),
+                };
+                Ok(Value::Num(x.abs()))
+            }),
+        );
+
+        env.insert_func(
+            "sin",
+            Rc::new(|args| {
+                if args.len() != 1 {
+                    return Err("sin function expects one argument.".into());
+                }
+                if let Value::Num(n) = &args[0] {
+                    Ok(Value::Num(n.sin()))
+                } else {
+                    Err("sin expects a float.".into())
+                }
+            }),
+        );
+
+        env.insert_func(
+            "cos",
+            Rc::new(|args| {
+                if args.len() != 1 {
+                    return Err("cos function expects one argument.".into());
+                }
+                if let Value::Num(n) = &args[0] {
+                    Ok(Value::Num(n.cos()))
+                } else {
+                    Err("cos expects a float.".into())
+                }
+            }),
+        );
+
+        env.insert("pi", Value::Num(PI));
+        env.insert("e", Value::Num(E));
+
+        env.insert_func(
+            ">",
+            Rc::new(|args| {
+                if args.len() != 2 {
+                    return Err("gt expects two arguments.".into());
+                }
+                let a = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("gt expects numeric arguments.".into()),
+                };
+                let b = match &args[1] {
+                    Value::Num(n) => *n,
+                    _ => return Err("gt expects numeric arguments.".into()),
+                };
+                Ok(Value::Num(if a > b { 1.0 } else { 0.0 }))
+            }),
+        );
+        env.insert_func(
+            "<",
+            Rc::new(|args| {
+                if args.len() != 2 {
+                    return Err("lt expects two arguments.".into());
+                }
+                let a = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("lt expects numeric arguments.".into()),
+                };
+                let b = match &args[1] {
+                    Value::Num(n) => *n,
+                    _ => return Err("lt expects numeric arguments.".into()),
+                };
+                Ok(Value::Num(if a < b { 1.0 } else { 0.0 }))
+            }),
+        );
+        env.insert_func(
+            ">=",
+            Rc::new(|args| {
+                if args.len() != 2 {
+                    return Err("gte expects two arguments.".into());
+                }
+                let a = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("gte expects numeric arguments.".into()),
+                };
+                let b = match &args[1] {
+                    Value::Num(n) => *n,
+                    _ => return Err("gte expects numeric arguments.".into()),
+                };
+                Ok(Value::Num(if a >= b { 1.0 } else { 0.0 }))
+            }),
+        );
+        env.insert_func(
+            "<=",
+            Rc::new(|args| {
+                if args.len() != 2 {
+                    return Err("lte expects two arguments.".into());
+                }
+                let a = match &args[0] {
+                    Value::Num(n) => *n,
+                    _ => return Err("lte expects numeric arguments.".into()),
+                };
+                let b = match &args[1] {
+                    Value::Num(n) => *n,
+                    _ => return Err("lte expects numeric arguments.".into()),
+                };
+                Ok(Value::Num(if a <= b { 1.0 } else { 0.0 }))
+            }),
+        );
+
+        env
+    }
 }
